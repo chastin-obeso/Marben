b30bb77a3d64c6e2a76e4d1fe3fd7c5dce215d55
Chastin Obeso <ckpobeso@gmail.com>
2025-09-18 07:51:25 +0800

    typo

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,7 +62,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
     public function canAccessPanel(Panel $panel): bool
     {
         return $this->status === 1;
-
+    }
     public function role() 
     {
         return $this->belongsTo(Role::class, 'role');
