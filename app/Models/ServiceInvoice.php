1ded0d3dfdf518c4b8e864e96fb03ef0ea289c2d
Chastin Obeso <ckpobeso@gmail.com>
2025-10-14 12:25:28 +0800

    Service Invoice Model

--- app/Models/ServiceInvoice.php
+++ app/Models/ServiceInvoice.php
@@ -2,35 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Models\Contracts\HasAvatar;
-use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
-
-class ServiceInvoice extends Authenticatable implements FilamentUser, HasAvatar
+use Illuminate\Database\Eloquent\Model;
+class ServiceInvoice extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
-
     /*
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'username',
-        'password',
-        'phone',
-        'status',
-        'role',
-        'service_types'
+        'service_invoice_number',
+        'service_invoice_series',
+        'amount_paid',
+        'payment_type',
+        'payment_date',
+        'bill_id'
     ];
 
     /**
@@ -39,8 +25,7 @@ class ServiceInvoice extends Authenticatable implements FilamentUser, HasAvatar
      * @var list<string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+
     ];
 
     /**
@@ -51,26 +36,14 @@ class ServiceInvoice extends Authenticatable implements FilamentUser, HasAvatar
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'service_types' => 'array'
+
         ];
-    }
-
-    public function getFilamentAvatarUrl(): ?string
-    {
-        return $this->avatar_url;
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return $this->role === 'admin' || true;
     }
 
     // Relationship section
 
-    public function serviceTypes(): BelongsToMany
+    public function bill()
     {
-        return $this->belongsToMany(ServiceType::class);
+        return $this->belongsTo(Bill::class);
     }
 }
